5b21b88dcfe6932b20532ea04c937248b205305b
李杰 <lj706517903@163.com>
2017-11-29 23:47:27 +0800

    完成个人中心——用户资料

--- src/pages/user/profileEdit.js
+++ src/pages/user/profileEdit.js
@@ -14,16 +14,43 @@ import {
     Card,
     Col,
     Upload,
+    Radio,
+    Cascader,
 } from "antd";
 import styles from "../../styles/user/profileEdit.css";
 import Header from "../../component/index/header";
 import Page from "../../component/index/page";
 
+const RadioGroup = Radio.Group;
 
-
+const options = [{
+  value: 'zhejiang',
+  label: 'Zhejiang',
+  children: [{
+    value: 'hangzhou',
+    label: 'Hangzhou',
+    children: [{
+      value: 'xihu',
+      label: 'West Lake',
+    }],
+  }],
+}, {
+  value: 'jiangsu',
+  label: 'Jiangsu',
+  children: [{
+    value: 'nanjing',
+    label: 'Nanjing',
+    children: [{
+      value: 'zhonghuamen',
+      label: 'Zhong Hua Men',
+    }],
+  }],
+}];
 
 class ProfileEdit extends Component {
+
     render() {
+
         return (
             <div>
                 <Header />
@@ -45,6 +72,36 @@ class ProfileEdit extends Component {
                                     </View>
                                 </View>
                             </Upload>
+                            <View className={styles.view6}>
+                                <span className={styles.span2}>李杰</span>
+                                <ShowEdit
+                                    title = {'昵称'}
+                                    value = {'李杰'}
+                                >
+                                    <Input placeholder="填写你的昵称" size={'large'} style={{width:200}}/>
+                                </ShowEdit>
+                                <ShowEdit
+                                    title = {'性别'}
+                                    value = {'男'}
+                                >
+                                    <RadioGroup size={'large'}>
+                                        <Radio value={1}>男</Radio>
+                                        <Radio value={2}>女</Radio>
+                                    </RadioGroup>
+                                </ShowEdit>
+                                <ShowEdit
+                                    title = {'一句话介绍'}
+                                    value = {null}
+                                >
+                                    <Input placeholder="填写你的介绍" style={{width:'70%'}} size={'large'}/>
+                                </ShowEdit>
+                                <ShowEdit
+                                    title = {'城市'}
+                                    value = {null}
+                                >
+                                    <Cascader options={options} placeholder="请选择你的城市" style={{width:300}}/>
+                                </ShowEdit>
+                            </View>
                         </View>
                     </Card>
                 </Page>
@@ -52,6 +109,103 @@ class ProfileEdit extends Component {
         );
     }
 }
+
+
+class ShowEdit extends Component {
+    state = {
+        showEditContent: false,
+        showEditButton: false,
+    }
+    render(){
+        const {
+            showEditContent,
+            showEditButton,
+        } = this.state
+        const {
+            title,
+            children,
+            value,
+        } = this.props
+        return(
+            <View
+                className={styles.view7}
+                onMouseEnter = {()=>{
+                    this.setState({
+                        showEditButton: true
+                    })
+                }}
+                onMouseLeave = {()=>{
+                    this.setState({
+                        showEditButton: false
+                    })
+                }}
+            >
+                <span className={styles.span3}>{title}</span>
+                <View className={styles.view8}>
+                    {
+                        showEditContent
+                        ?   <View style={{flex:1}}>
+                                {children}
+                                <div className={styles.div1}>
+                                    <Button
+                                        type="primary"
+                                        size={'large'}
+                                        onClick = {()=>{
+                                            this.setState({
+                                                showEditContent: false
+                                            })
+                                        }}
+                                    >
+                                        保存
+                                    </Button>
+                                    <Button
+                                        style={{marginLeft:15}}
+                                        size={'large'}
+                                        onClick = {()=>{
+                                            this.setState({
+                                                showEditContent: false
+                                            })
+                                        }}
+                                    >
+                                        取消
+                                    </Button>
+                                </div>
+                            </View>
+                        :   value
+                            ?   <div>
+                                    <span className={styles.span4}>{value}</span>
+                                    {
+                                        showEditButton&&
+                                        <Button
+                                            icon="edit"
+                                            onClick = {()=>{
+                                                this.setState({
+                                                    showEditContent: true
+                                                })
+                                            }}
+                                            style = {{marginLeft:20}}
+                                        >
+                                            修改
+                                        </Button>
+                                    }
+                                </div>
+                            :   <Button
+                                    icon="edit"
+                                    onClick = {()=>{
+                                        this.setState({
+                                            showEditContent: true
+                                        })
+                                    }}
+                                >
+                                    填写
+                                </Button>
+                    }
+                </View>
+            </View>
+        )
+    }
+}
+
 
 const indexViewFunc = store => {
     const { shopCartIndex } = store.view;
